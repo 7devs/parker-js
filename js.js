9fdf56cb0f4259678f3f4525ae307f2aef842f0b
Jiang Shan <jiangshan2010@vip.sina.com>
2016-09-05 19:40:02 +0800

    @@

---
+++ js.js
@@ -0,0 +1,39 @@
+var arr = new Array(6)
+arr[0] = "George"
+arr[1] = "John"
+arr[2] = "Thomas"
+arr[3] = "James"
+arr[4] = "Adrew"
+arr[5] = "Martin"
+
+document.write(arr + "<br/>")
+arr.splice(2,0,"William")
+document.write(arr + "<br/>")
+
+var arr = new Array(6)
+arr[0] = "George"
+arr[1] = "John"
+arr[2] = "Thomas"
+arr[3] = "James"
+arr[4] = "Adrew"
+arr[5] = "Martin"
+
+document.write(arr + "<br/>")
+arr.splice(2,1,"William")
+document.write(arr + "<br/>")
+
+
+var arr = new Array(6)
+arr[0] = "George"
+arr[1] = "John"
+arr[2] = "Thomas"
+arr[3] = "James"
+arr[4] = "Adrew"
+arr[5] = "Martin"
+
+document.write(arr + "<br/>")
+arr.splice(2,3,"William")
+document.write(arr + "<br/>")
+
+var str ='hello node js';
+var arr = [1,2,3,4];
